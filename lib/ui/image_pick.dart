0c913f5b1facc11cad258af5a89544bf235e318c
anusornleo <60070110@kmitl.ac.th>
2019-05-17 21:33:24 +0700

    add

--- lib/ui/image_pick.dart
+++ lib/ui/image_pick.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
+import 'dart:async';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:intl/intl.dart';
@@ -13,48 +15,101 @@ class CameraScreen extends StatefulWidget {
   }
 }
 
+// class CameraScreenState extends State {
+//   File _image;
+
+//   Future getImage() async {
+//     var image = await ImagePicker.pickImage(source: ImageSource.camera);
+//     setState(() {
+//       _image = image;
+//     });
+//   }
+
+//   Future getImageFromPhone() async {
+//     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+//     setState(() {
+//       _image = image;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           "Image picker",
+//         ),
+//       ),
+//       body: Form(
+//         child: ListView(
+//           children: <Widget>[
+//             _image == null ? Text("No image selected") : Image.file(_image),
+//             RaisedButton(child: Icon(Icons.camera),
+//               onPressed: () {
+//                 getImage();
+//               },
+//             ),
+//             RaisedButton(child: Icon(Icons.add),
+//               onPressed: () {
+//                 getImageFromPhone();
+//               },
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class CameraScreenState extends State {
-  File _image;
+  File sampleImage;
 
   Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
-    setState(() {
-      _image = image;
-    });
-  }
+    var tempImage = await ImagePicker.pickImage(source: ImageSource.gallery);
 
-  Future getImageFromPhone() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
     setState(() {
-      _image = image;
+      sampleImage = tempImage;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Image picker",
-        ),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Image Upload'),
+        centerTitle: true,
       ),
-      body: Form(
-        child: ListView(
-          children: <Widget>[
-            _image == null ? Text("No image selected") : Image.file(_image),
-            RaisedButton(child: Icon(Icons.camera),
-              onPressed: () {
-                getImage();
-              },
-            ),
-            RaisedButton(child: Icon(Icons.add),
-              onPressed: () {
-                getImageFromPhone();
-              },
-            )
-          ],
-        ),
+      body: new Center(
+        child: sampleImage == null ? Text('Select an image') : enableUpload(),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Add Image',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget enableUpload() {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Image.file(sampleImage, height: 300.0, width: 300.0),
+          Text(sampleImage.toString()),
+          RaisedButton(
+            elevation: 7.0,
+            child: Text('Upload'),
+            textColor: Colors.white,
+            color: Colors.blue,
+            onPressed: () {
+              final StorageReference firebaseStorageRef =
+                  FirebaseStorage.instance.ref().child('myimage.jpg');
+              final StorageUploadTask task =
+                  firebaseStorageRef.putFile(sampleImage);
+            },
+          )
+        ],
       ),
     );
   }
